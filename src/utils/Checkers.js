b8bfc19d79be1ac297bd2bf7e6491d996b0044d7
Michael van Meerwijk <michaelvanm@gmail.com>
2018-11-27 16:39:52 +0100

    Random AI makes a random move

--- src/utils/Checkers.js
+++ src/utils/Checkers.js
@@ -1,15 +1,19 @@
-import { RULES } from './constants.js';
+import { RULES, PLAYER_BLACK, PLAYER_WHITE } from './constants.js';
 import Moves from './Moves.js';
 import GameState from './GameState.js';
+import { HumanPlayer, RandomAI } from './Player.js';
 
 export default class Checkers {
-	constructor(options = RULES) {
+	constructor(options = RULES, WhiteClass = RandomAI, BlackClass = RandomAI) {
 
 		this.size = options.size;
 		this.firstMove = options.firstMove;
 		this.captureBack = options.captureBack;
 		this.flyingKings = options.flyingKings;
 		this.autoDrawAfterNoCaptures = options.autoDrawAfterNoCaptures;
+
+		this.playerWhite = new WhiteClass(PLAYER_WHITE, this.makeMove.bind(this));
+		this.playerBlack = new BlackClass(PLAYER_BLACK, this.makeMove.bind(this));
 
 		this.moves = new Moves(options);
 		this.gameState = new GameState(
@@ -32,6 +36,31 @@ export default class Checkers {
 
 		console.warn({ gameOver: this.gameState.isGameOver() });
 
+		if (this.gameState.gameOver) return;
+
+		if (this.currentPlayerIsAI) {
+			return this.requestAiMove();
+		} else {
+			return this;
+		}
+	}
+
+	currentPlayerClass() {
+		if (this.gameState.currentPlayer === PLAYER_BLACK) return this.playerBlack;
+		else return this.playerWhite;
+	}
+
+	currentPlayerIsAI() {
+		return !this.currentPlayerClass.isHuman;
+	}
+
+	requestAiMove() {
+		let player = this.currentPlayerClass();
+
+		player.setValidMoves(this.moves.validMoves);
+		setTimeout(() => {
+			player.makeMove();
+		}, 1000);
 		return this;
 	}
 
---
+++ src/utils/Player.js
@@ -0,0 +1,48 @@
+class PlayerInterface {
+	constructor(color, isHuman, sendMove) {
+		this.color = color;
+		this.validMoves = [];
+		this.isHuman = isHuman;
+		this.sendMove = sendMove;
+	}
+
+	setValidMoves(validMoves) {
+		this.validMoves = validMoves;
+		return this;
+	}
+
+	findValidMovePaths() {
+		let paths = [];
+
+		this.validMoves.forEach(piecePaths => {
+			piecePaths.moves.forEach(path => {
+				paths.push({ from: piecePaths.piece, moves: path });
+			})
+		})
+		return paths;
+	}
+}
+
+export class RandomAI extends PlayerInterface {
+	constructor(color, sendMove) {
+		super(color, false, sendMove);
+	}
+
+	makeMove() {
+		if (this.validMoves.length <= 0) return;
+
+		const validMovePaths = this.findValidMovePaths();
+		const path = validMovePaths[Math.floor(Math.random() * validMovePaths.length)];
+
+		const from = path.from;
+		const to = path.moves[path.moves.length - 1];
+
+		this.sendMove(from.x, from.y, to.x, to.y);
+	}
+}
+
+export class HumanPlayer extends PlayerInterface {
+	constructor(color, sendMove) {
+		super(color, true, sendMove);
+	}
+}
